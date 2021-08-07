816f04e3d7e3fa30c65d6fcecc5a8b9b18634d55
Troy Sornson <troy@sornson.io>
2021-08-07 11:28:01 -0600

    Index page is now slangified

--- site/src/site.cr
+++ site/src/site.cr
@@ -1,9 +1,15 @@
 require "slang"
 
-Dir.glob("./**.slang") do |template|
-  File.open("../#{File.basename(template, ".slang")}.html", "w") do |file|
-    file.puts(String.build do |__str__|
-      Slang.process_file(template, "__str__")
-    end)
-  end
-end
+{% for file_name in `ls src/pages/*.slang`.split("\n").map(&.id) %}
+  {% if "#{file_name}".size > 0 %}
+    puts("Processing {{file_name}}")
+    File.open("../#{File.basename("{{file_name}}", ".slang")}.html", "w") do |file|
+      output = String.build do |__str__|
+        Slang.embed("{{file_name}}", "__str__")
+      end
+
+      puts(output)
+      file.puts(output)
+    end
+  {% end %}
+{% end %}
